13f9c05c1988f058ad2522888b1ed1117b6165ce
Aga Daniuk <agdani@wp.pl>
2021-03-29 14:45:02 +0200

    Add project design input

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
+
+const upload = multer();
 const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -22,6 +27,16 @@ app.get('/about', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('file'), (req, res, next) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file.originalname) {
+    res.render('contact', { isSent: true, name: req.file.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.get('/info', (req, res) => {
